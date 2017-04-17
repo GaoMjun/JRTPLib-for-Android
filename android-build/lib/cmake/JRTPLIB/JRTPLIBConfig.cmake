4f12473f41d555083f8c4f4687dc76908ee3c2d2
GaoMjun <jun2675731483@gmail.com>
2017-04-17 23:12:03 +0800

    build

---
+++ android-build/lib/cmake/JRTPLIB/JRTPLIBConfig.cmake
@@ -0,0 +1,7 @@
+
+set(JRTPLIB_FOUND 1)
+
+set(JRTPLIB_INCLUDE_DIRS /Users/mannix/SourceTree/JRTPLib/app/src/main/cpp/JRTPLib-for-Android/JThread-Android/android-build/include;/Users/mannix/SourceTree/JRTPLib/app/src/main/cpp/JRTPLib-for-Android/android-build/include)
+
+set(JRTPLIB_LIBRARIES optimized;/Users/mannix/SourceTree/JRTPLib/app/src/main/cpp/JRTPLib-for-Android/android-build/lib/libjrtplib.a;debug;/Users/mannix/SourceTree/JRTPLib/app/src/main/cpp/JRTPLib-for-Android/android-build/lib/libjrtplib.a;optimized;/Users/mannix/SourceTree/JRTPLib/app/src/main/cpp/JRTPLib-for-Android/JThread-Android/android-build/lib/libjthread.a;debug;ws2_32)
+
